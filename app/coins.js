81579d4de94cde362875d2bc228a6b1395944d5d
Bohdan Vanieiev <warchantua@gmail.com>
2020-12-09 19:37:47 +0200

    Add PHL specifics

--- app/coins.js
+++ app/coins.js
@@ -1,11 +1,13 @@
 var btc = require("./coins/btc.js");
 var ltc = require("./coins/ltc.js");
 var vbtc = require("./coins/vbtc.js");
+var phl = require("./coins/phl.js");
 
 module.exports = {
 	"BTC": btc,
 	"LTC": ltc,
 	"vBTC": vbtc,
+	"PHL": phl,
 
-	"coins":["BTC", "LTC", "vBTC"]
+	"coins":["BTC", "LTC", "vBTC", "PHL"]
 };
