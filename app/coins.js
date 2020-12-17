0a66a7d36dac7837d397179148525b80a71210f0
Bohdan Vanieiev <warchantua@gmail.com>
2020-12-17 19:38:58 +0200

    Add PEXA

--- app/coins.js
+++ app/coins.js
@@ -2,12 +2,14 @@ var btc = require("./coins/btc.js");
 var ltc = require("./coins/ltc.js");
 var vbtc = require("./coins/vbtc.js");
 var phl = require("./coins/phl.js");
+var pexa = require("./coins/pexa.js");
 
 module.exports = {
 	"BTC": btc,
 	"LTC": ltc,
 	"vBTC": vbtc,
 	"PHL": phl,
+	"PEXA": pexa,
 
-	"coins":["BTC", "LTC", "vBTC", "PHL"]
+	"coins":["BTC", "LTC", "vBTC", "PHL", "PEXA"]
 };
