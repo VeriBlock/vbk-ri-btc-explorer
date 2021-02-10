c7ecd55f416c8d76a726143815cdfe39aaa78d48
Andrew Lekar <andrey_lekar@adoriasoft.com>
2021-02-10 16:10:59 +0000

    Add BITC

--- app/api/rpcApi.js
+++ app/api/rpcApi.js
@@ -25,7 +25,7 @@ var rpcQueue = async.queue(function(task, callback) {
 
 }, config.rpcConcurrency);
 
-var minRpcVersions = {getblockstats:"0.17.0"};
+var minRpcVersions = {getblockstats:"0.18.0"};
 
 global.rpcStats = {};
 
--- app/coins.js
+++ app/coins.js
@@ -3,6 +3,7 @@ var ltc = require("./coins/ltc.js");
 var vbtc = require("./coins/vbtc.js");
 var phl = require("./coins/phl.js");
 var pexa = require("./coins/pexa.js");
+var bitc = require("./coins/bitc.js");
 
 module.exports = {
 	"BTC": btc,
@@ -10,6 +11,7 @@ module.exports = {
 	"vBTC": vbtc,
 	"PHL": phl,
 	"PEXA": pexa,
+	"BITC": bitc,
 
-	"coins":["BTC", "LTC", "vBTC", "PHL", "PEXA"]
+	"coins":["BTC", "LTC", "vBTC", "PHL", "PEXA", "BITC"]
 };
