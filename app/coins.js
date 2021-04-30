b73aa129547738336665a0ddc57dab78041a6426
Overcooked Panda <overcookedpanda@gmail.com>
2021-04-30 10:28:48 -0500

    remove BITC

--- app/coins.js
+++ app/coins.js
@@ -3,7 +3,6 @@ var ltc = require("./coins/ltc.js");
 var vbtc = require("./coins/vbtc.js");
 var phl = require("./coins/phl.js");
 var pexa = require("./coins/pexa.js");
-var bitc = require("./coins/bitc.js");
 
 module.exports = {
 	"BTC": btc,
@@ -11,7 +10,6 @@ module.exports = {
 	"vBTC": vbtc,
 	"PHL": phl,
 	"PEXA": pexa,
-	"BITC": bitc,
 
-	"coins":["BTC", "LTC", "vBTC", "PHL", "PEXA", "BITC"]
+	"coins":["BTC", "LTC", "vBTC", "PHL", "PEXA"]
 };
