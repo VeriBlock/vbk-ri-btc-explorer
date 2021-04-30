04dc9c4aedeed862a73afdc07601ae8e05b4ee00
Overcooked Panda <overcookedpanda@gmail.com>
2021-04-30 08:36:13 -0500

    add BITC to explorer

---
+++ app/coins/bitc.js
@@ -0,0 +1,308 @@
+var Decimal = require("decimal.js");
+Decimal8 = Decimal.clone({ precision:8, rounding:8 });
+
+var currencyUnits = [
+    {
+        type:"native",
+        name:"BITC",
+        multiplier:1,
+        default:true,
+        values:["", "bitc", "BITC"],
+        decimalPlaces:8
+    },
+    {
+        type:"native",
+        name:"mBITC",
+        multiplier:1000,
+        values:["mbtc"],
+        decimalPlaces:5
+    },
+    {
+        type:"native",
+        name:"bits",
+        multiplier:1000000,
+        values:["bits"],
+        decimalPlaces:2
+    },
+    {
+        type:"native",
+        name:"sat",
+        multiplier:100000000,
+        values:["sat", "satoshi"],
+        decimalPlaces:0
+    },
+    {
+        type:"exchanged",
+        name:"USD",
+        multiplier:"usd",
+        values:["usd"],
+        decimalPlaces:2,
+        symbol:"$"
+    },
+    {
+        type:"exchanged",
+        name:"EUR",
+        multiplier:"eur",
+        values:["eur"],
+        decimalPlaces:2,
+        symbol:"€"
+    },
+];
+
+module.exports = {
+    name:"BitCash",
+    ticker:"BITC",
+    logoUrlsByNetwork:{
+        "main":"/img/logo/bitc.svg",
+        "test":"/img/logo/bitc.svg",
+        "regtest":"/img/logo/bitc.svg"
+    },
+    siteTitlesByNetwork: {
+        "main":"Mainnet PoP BitCash Explorer",
+        "test":"Testnet PoP BitCash Explorer",
+        "regtest":"Regtest PoP BitCash Explorer"
+    },
+    siteDescriptionHtml:"<b>BITC Explorer</b> is <a href='https://github.com/janoside/btc-rpc-explorer). If you run your own [Bitcoin Full Node](https://bitcoin.org/en/full-node), **BTC Explorer** can easily run alongside it, communicating via RPC calls. See the project [ReadMe](https://github.com/janoside/btc-rpc-explorer) for a list of features and instructions for running.",
+    nodeTitle:"BitCash Full Node",
+    nodeUrl:"https://github.com/VeriBlock/pop-bitc",
+    demoSiteUrl: "",
+    miningPoolsConfigUrls:[
+    ],
+    maxBlockWeight: 4000000,
+    maxBlockSize: 1000000,
+    difficultyAdjustmentBlockCount: 2016,
+    maxSupplyByNetwork: {
+        "main": new Decimal(20999817.31308491), // ref: https://bitcoin.stackexchange.com/a/38998
+        "test": new Decimal(21000000),
+        "regtest": new Decimal(21000000)
+    },
+    targetBlockTimeSeconds: 600,
+    targetBlockTimeMinutes: 10,
+    currencyUnits:currencyUnits,
+    currencyUnitsByName:{"BITC":currencyUnits[0], "mBITC":currencyUnits[1], "bits":currencyUnits[2], "sat":currencyUnits[3]},
+    baseCurrencyUnit:currencyUnits[3],
+    defaultCurrencyUnit:currencyUnits[0],
+    feeSatoshiPerByteBucketMaxima: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 50, 75, 100, 150],
+    genesisBlockHashesByNetwork:{
+        "main":    "7d57d87ff3c15a521530af60edee1887fba9c193eb518face926785c4cd8f4f1",
+        "test":    "50d97c38f8a673c4758718d4900bef9192f0318a8aa9ee415eeb27c3c431ff43",
+        "regtest": "50d97c38f8a673c4758718d4900bef9192f0318a8aa9ee415eeb27c3c431ff43"
+    },
+    genesisCoinbaseTransactionIdsByNetwork: {
+        "main":    "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
+        "test":    "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
+        "regtest": "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"
+    },
+    genesisCoinbaseTransactionsByNetwork:{
+        "main": {
+            "hex": "01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0804ffff001d02fd04ffffffff0100f2052a01000000434104f5eeb2b10c944c6b9fbcfff94c35bdeecd93df977882babc7f3a2cf7f5c81d3b09a68db7f0e04f21de5d4230e75e6dbe7ad16eefe0d4325a62067dc6f369446aac00000000",
+            "txid": "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
+            "hash": "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
+            "size": 204,
+            "vsize": 204,
+            "version": 1,
+            "confirmations":475000,
+            "vin": [
+                {
+                    "coinbase": "04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73",
+                    "sequence": 4294967295
+                }
+            ],
+            "vout": [
+                {
+                    "value": 50,
+                    "n": 0,
+                    "scriptPubKey": {
+                        "asm": "04f5eeb2b10c944c6b9fbcfff94c35bdeecd93df977882babc7f3a2cf7f5c81d3b09a68db7f0e04f21de5d4230e75e6dbe7ad16eefe0d4325a62067dc6f369446a OP_CHECKSIG",
+                        "hex": "4104f5eeb2b10c944c6b9fbcfff94c35bdeecd93df977882babc7f3a2cf7f5c81d3b09a68db7f0e04f21de5d4230e75e6dbe7ad16eefe0d4325a62067dc6f369446aac",
+                        "reqSigs": 1,
+                        "type": "pubkey",
+                        "addresses": [
+                            "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa"
+                        ]
+                    }
+                }
+            ],
+            "blockhash": "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
+            "time": 1230988505,
+            "blocktime": 1230988505
+        },
+        "test": {
+            "hex": "01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73ffffffff0100f2052a01000000434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac00000000",
+            "txid": "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
+            "hash": "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
+            "version": 1,
+            "size": 204,
+            "vsize": 204,
+            "weight": 816,
+            "locktime": 0,
+            "vin": [
+                {
+                    "coinbase": "04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73",
+                    "sequence": 4294967295
+                }
+            ],
+            "vout": [
+                {
+                    "value": 50.00000000,
+                    "n": 0,
+                    "scriptPubKey": {
+                        "asm": "04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f OP_CHECKSIG",
+                        "hex": "4104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac",
+                        "reqSigs": 1,
+                        "type": "pubkey",
+                        "addresses": [
+                            "mpXwg4jMtRhuSpVq4xS3HFHmCmWp9NyGKt"
+                        ]
+                    }
+                }
+            ],
+            "blockhash": "000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943",
+            "time": 1296688602,
+            "blocktime": 1296688602
+        },
+        "regtest": {
+            "hex": "01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73ffffffff0100f2052a01000000434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac00000000",
+            "txid": "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
+            "hash": "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b",
+            "version": 1,
+            "size": 204,
+            "vsize": 204,
+            "weight": 816,
+            "locktime": 0,
+            "vin": [
+                {
+                    "coinbase": "04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73",
+                    "sequence": 4294967295
+                }
+            ],
+            "vout": [
+                {
+                    "value": 50.00000000,
+                    "n": 0,
+                    "scriptPubKey": {
+                        "asm": "04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f OP_CHECKSIG",
+                        "hex": "4104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac",
+                        "type": "pubkey"
+                    }
+                }
+            ],
+            "blockhash": "0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206",
+            "time": 1296688602,
+            "blocktime": 1296688602
+        }
+    },
+    genesisBlockStatsByNetwork:{
+        "main": {
+            "avgfee": 0,
+            "avgfeerate": 0,
+            "avgtxsize": 0,
+            "blockhash": "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
+            "feerate_percentiles": [
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            "height": 0,
+            "ins": 0,
+            "maxfee": 0,
+            "maxfeerate": 0,
+            "maxtxsize": 0,
+            "medianfee": 0,
+            "mediantime": 1231006505,
+            "mediantxsize": 0,
+            "minfee": 0,
+            "minfeerate": 0,
+            "mintxsize": 0,
+            "outs": 1,
+            "subsidy": 5000000000,
+            "swtotal_size": 0,
+            "swtotal_weight": 0,
+            "swtxs": 0,
+            "time": 1231006505,
+            "total_out": 0,
+            "total_size": 0,
+            "total_weight": 0,
+            "totalfee": 0,
+            "txs": 1,
+            "utxo_increase": 1,
+            "utxo_size_inc": 117
+        },
+        "test": {
+            "avgfee": 0,
+            "avgfeerate": 0,
+            "avgtxsize": 0,
+            "blockhash": "000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943",
+            "feerate_percentiles": [
+                0,
+                0,
+                0,
+                0,
+                0
+            ],
+            "height": 0,
+            "ins": 0,
+            "maxfee": 0,
+            "maxfeerate": 0,
+            "maxtxsize": 0,
+            "medianfee": 0,
+            "mediantime": 1296688602,
+            "mediantxsize": 0,
+            "minfee": 0,
+            "minfeerate": 0,
+            "mintxsize": 0,
+            "outs": 1,
+            "subsidy": 5000000000,
+            "swtotal_size": 0,
+            "swtotal_weight": 0,
+            "swtxs": 0,
+            "time": 1296688602,
+            "total_out": 0,
+            "total_size": 0,
+            "total_weight": 0,
+            "totalfee": 0,
+            "txs": 1,
+            "utxo_increase": 1,
+            "utxo_size_inc": 117
+        }
+    },
+    genesisCoinbaseOutputAddressScripthash:"8b01df4e368ea28f8dc0423bcf7a4923e3a12d307c875e47a0cfbf90b5c39161",
+    historicalData: [
+    ],
+    exchangeRateData:{
+        jsonUrl:"https://api.coindesk.com/v1/bpi/currentprice.json",
+        responseBodySelectorFunction:function(responseBody) {
+            //console.log("Exchange Rate Response: " + JSON.stringify(responseBody));
+
+            var exchangedCurrencies = ["USD", "GBP", "EUR"];
+
+            if (responseBody.bpi) {
+                var exchangeRates = {};
+
+                for (var i = 0; i < exchangedCurrencies.length; i++) {
+                    if (responseBody.bpi[exchangedCurrencies[i]]) {
+                        exchangeRates[exchangedCurrencies[i].toLowerCase()] = responseBody.bpi[exchangedCurrencies[i]].rate_float;
+                    }
+                }
+
+                return exchangeRates;
+            }
+
+            return null;
+        }
+    },
+    blockRewardFunction:function(blockHeight, chain) {
+        var eras = [ new Decimal8(50) ];
+        for (var i = 1; i < 34; i++) {
+            var previous = eras[i - 1];
+            eras.push(new Decimal8(previous).dividedBy(2));
+        }
+
+        var halvingBlockInterval = (chain == "regtest" ? 150 : 210000);
+        var index = Math.floor(blockHeight / halvingBlockInterval);
+
+        return eras[index];
+    }
+};
